a3121aaec68c248f37adb4b6ad859fb22a8a01bd
E3tow1C <thepphithak.w@kkumail.com>
2025-06-14 11:25:25 +0700

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+from typing import Any
+import httpx
+from mcp.server.fastmcp import FastMCP
+
+
+mcp - FastMCP("ms-products")
+mock_api_url = "https://589d8ea5400db4120026146d.mockapi.io/api/products"
+
+
+@mcp.tool()
+def get_products():
+    response = httpx.get(mock_api_url)
+    return response.json()
+
+if __name__ == "__main__":
+    mcp.run(transport="stdin")
